11180bcffe30d1feb7a3a2f66dbe5a4a8a3c6e12
jongmin-dev <chungjm0711@gmail.com>
2022-06-28 16:57:56 +0900

    해당 포스트로 Link를 이용하여 이동

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,33 +1,34 @@
-import { graphql, useStaticQuery } from "gatsby";
+import { graphql, Link } from "gatsby";
 import React from "react";
 import Layout from "../../components/Layout";
-import { MDXRenderer } from "gatsby-plugin-mdx";
 
-function BlogPage() {
-  const data = useStaticQuery(graphql`
-    query {
-      allMdx(sort: { fields: frontmatter___date, order: DESC }) {
-        nodes {
-          frontmatter {
-            date(formatString: "MMMM D, YYYY")
-            title
-          }
-          id
-          body
-        }
-      }
-    }
-  `);
+const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My Blog Posts">
       {data.allMdx.nodes.map((node) => (
         <article key={node.id}>
-          <h2>{node.frontmatter.title}</h2>
+          <h2>
+            <Link to={`/blog/${node.slug}`}>{node.frontmatter.title}</Link>
+          </h2>
           <p>Posted: {node.frontmatter.date}</p>
-          <MDXRenderer>{node.body}</MDXRenderer>
         </article>
       ))}
     </Layout>
   );
-}
+};
+
+export const query = graphql`
+  query {
+    allMdx(sort: { fields: frontmatter___date, order: DESC }) {
+      nodes {
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
+        }
+        id
+        slug
+      }
+    }
+  }
+`;
 export default BlogPage;

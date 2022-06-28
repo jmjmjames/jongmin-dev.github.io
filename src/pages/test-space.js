6e92d2b774e30db8de61104bfca4c4737ec54abc
jongmin-dev <chungjm0711@gmail.com>
2022-06-28 10:23:32 +0900

    test 페이지 추가

---
+++ src/pages/test-space.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+// markup
+const TestSpacePage = () => {
+  return (
+    <main>
+      <title>Test-Space Page</title>
+      <h1>Test-Space Page</h1>
+    </main>
+  );
+};
+
+export default TestSpacePage;
---
+++ src/pages/test.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+// markup
+const TestPage = () => {
+  return (
+    <main>
+      <title>Test Page</title>
+      <h1>Test Page</h1>
+    </main>
+  );
+};
+
+export default TestPage;

6853ac51653123dc858a0bcb93afd7e9e440881f
jongmin-dev <chungjm0711@gmail.com>
2022-06-28 11:02:05 +0900

    page link test

--- src/pages/test-space.js
+++ src/pages/test-space.js
@@ -1,11 +1,22 @@
+import { Link } from "gatsby";
 import React from "react";
 
 // markup
 const TestSpacePage = () => {
   return (
     <main>
-      <title>Test-Space Page</title>
-      <h1>Test-Space Page</h1>
+      <title>테스트 스페이스</title>
+      <div style={{ display: "flex" }}>
+        <Link to="/">로고</Link>
+        <div style={{ flexGrow: 1 }}></div>
+        <Link to="/test" style={{ padding: 10 }}>
+          테스트
+        </Link>
+        <Link to="/test-space" style={{ padding: 10 }}>
+          테스트 스페이스
+        </Link>
+      </div>
+      <h1>테스트 스페이스 페이지 입니다.</h1>
     </main>
   );
 };
--- src/pages/test.js
+++ src/pages/test.js
@@ -1,3 +1,4 @@
+import { Link } from "gatsby";
 import React from "react";
 
 // markup
@@ -5,6 +6,16 @@ const TestPage = () => {
   return (
     <main>
       <title>Test Page</title>
+      <div style={{ display: "flex" }}>
+        <Link to="/">로고</Link>
+        <div style={{ flexGrow: 1 }}></div>
+        <Link to="/test" style={{ padding: 10 }}>
+          테스트
+        </Link>
+        <Link to="/test-space" style={{ padding: 10 }}>
+          테스트 스페이스
+        </Link>
+      </div>
       <h1>Test Page</h1>
     </main>
   );
